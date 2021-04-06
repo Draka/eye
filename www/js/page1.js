8ef2442806695819a0e398c88b06a11942b05b12
Tarquino <andres.tarquino@accenture.com>
2021-04-06 17:55:32 -0500

    Added brush selection, and the rest of the animations - Pending for optimizing the code using a function to select the brush - Pending to delete all the files that are not being used - Added image saving functionality

--- www/js/page1.js
+++ www/js/page1.js
@@ -2,6 +2,7 @@ var pintando = false
 var pincel = null
 var step = 1
 var actions = false
+var emocion = null
 
 //Reproducción de la animación de ingreso
 lottie.loadAnimation({
@@ -71,7 +72,7 @@ $('#btn-continuar2').on('click', function(){
                 lottie.loadAnimation({
                     container: document.getElementById('3_instrucciones'),
                     renderer: 'svg',
-                    loop: false,
+                    loop: true,
                     autoplay: true,
                     path: '3_instrucciones.json'
                 })
@@ -115,33 +116,17 @@ $('#btn-continuar4').on('click', function () {
                 document.getElementById("plotting_canvas").style.setProperty('bottom', '0')
                 document.getElementById("plotting_canvas").style.setProperty('left', '0')
                 $('.content-page4').show('slow', function () {
+                    lottie.loadAnimation({
+                        container: document.getElementById('4_seleccion'),
+                        renderer: 'svg',
+                        loop: false,
+                        autoplay: true,
+                        path: '4_seleccion.json'
+                    })
                     actions = false
                     step = 6
                     if (step === 6) {
                         actions = true
-                        setTimeout(function () {
-                            $('.content-page4').hide('slow', function () {
-                                $('.content-page5').show('slow', function () {
-                                    document.getElementById("plotting_canvas").style.setProperty('background-color', '#73c0ba')
-                                    actions = false
-                                    step = 7
-                                    if (step === 7) {
-                                        setTimeout(function () {
-                                            $('.content-page5').hide('slow', function () {
-                                                document.getElementById("plotting_canvas").style.setProperty('background-color', '#ffffff')
-                                                actions = false
-                                                step = 8
-                                                // Logica de pincel aca, cuando seleccione ponga la función
-                                                pincel = pincel11
-                                                pincel.init()
-                                                pintando = true;
-                                                console.log('pintando', pintando)
-                                            })
-                                        }, 500)
-                                    }
-                                })
-                            })
-                        }, 2000)
                     }
                 })
             })
@@ -149,6 +134,271 @@ $('#btn-continuar4').on('click', function () {
     }
 })
 
+$('.ojo_feliz').on('click', function(){
+    if(step === 6){
+        actions = true
+        $('.content-page4').hide('slow', function(){
+            $('.content-page5').show('slow', function(){
+                document.getElementById("plotting_canvas").style.setProperty('background-color', '#73c0ba')
+                lottie.loadAnimation({
+                    container: document.getElementById('5_instrucciones'),
+                    renderer: 'svg',
+                    loop: false,
+                    autoplay: true,
+                    path: '5_instrucciones.json'
+                })
+                actions = false
+                step = 7
+                if(step === 7){
+                    actions = true
+                    setTimeout(function(){
+                        $('.content-page5').hide('slow', function () {
+                            document.getElementById("plotting_canvas").style.setProperty('background-color', '#ffffff')
+                            // Logica de pincel aca, cuando seleccione ponga la función
+                            pincel = pincel10
+                            pincel.init()
+                            pintando = true;
+                            console.log('pintando', pintando)
+                            actions = false
+                            step = 8
+                            if(step === 8){
+                                actions = true
+                                setTimeout(function(){
+                                    webgazer.pause()
+                                    $('.content-page6').show('slow', function(){
+                                        lottie.loadAnimation({
+                                            container: document.getElementById('6_gracias'),
+                                            renderer: 'svg',
+                                            loop: false,
+                                            autoplay: true,
+                                            path: '6_gracias.json'
+                                        })
+                                        setTimeout(function(){
+                                            const mc = document.querySelector("#plotting_canvas");
+                                            $('.page-btn5').on('click', function(){
+                                                if(window.navigator.msSaveBlob){
+                                                    window.navigator.msSaveBlob(mc.msToBlob(), "archivo.jpeg");
+                                                } else{
+                                                    const a = document.createElement("a");
+                                                    document.body.appendChild(a);
+                                                    a.href = mc.toDataURL();
+                                                    a.download = "archivo.jpeg";
+                                                    a.click();
+                                                    document.body.removeChild(a);
+                                                }
+                                            })
+                                        }, 7000)
+                                    })
+                                }, 30000)
+                            }
+                        })
+                    }, 9000)
+                }
+            })
+        })
+    }
+})
+
+$('.ojo_aburrido').on('click', function(){
+    if(step === 6){
+        actions = true
+        $('.content-page4').hide('slow', function(){
+            $('.content-page5').show('slow', function(){
+                document.getElementById("plotting_canvas").style.setProperty('background-color', '#73c0ba')
+                lottie.loadAnimation({
+                    container: document.getElementById('5_instrucciones'),
+                    renderer: 'svg',
+                    loop: false,
+                    autoplay: true,
+                    path: '5_instrucciones.json'
+                })
+                actions = false
+                step = 7
+                if(step === 7){
+                    actions = true
+                    setTimeout(function(){
+                        $('.content-page5').hide('slow', function () {
+                            document.getElementById("plotting_canvas").style.setProperty('background-color', '#ffffff')
+                            // Logica de pincel aca, cuando seleccione ponga la función
+                            pincel = pincel15
+                            pincel.init()
+                            pintando = true;
+                            console.log('pintando', pintando)
+                            actions = false
+                            step = 8
+                            if(step === 8){
+                                actions = true
+                                setTimeout(function(){
+                                    webgazer.pause()
+                                    $('.content-page6').show('slow', function(){
+                                        lottie.loadAnimation({
+                                            container: document.getElementById('6_gracias'),
+                                            renderer: 'svg',
+                                            loop: false,
+                                            autoplay: true,
+                                            path: '6_gracias.json'
+                                        })
+                                        setTimeout(function(){
+                                            const mc = document.querySelector("#plotting_canvas");
+                                            $('.page-btn5').on('click', function(){
+                                                if(window.navigator.msSaveBlob){
+                                                    window.navigator.msSaveBlob(mc.msToBlob(), "archivo.jpeg");
+                                                } else{
+                                                    const a = document.createElement("a");
+                                                    document.body.appendChild(a);
+                                                    a.href = mc.toDataURL();
+                                                    a.download = "archivo.jpeg";
+                                                    a.click();
+                                                    document.body.removeChild(a);
+                                                }
+                                            })
+                                        }, 7000)
+                                    })
+                                }, 30000)
+                            }
+                        })
+                    }, 9000)
+                    actions = false
+                    step = 8
+                }
+            })
+        })
+    }
+})
+
+$('.ojo_enojado').on('click', function(){
+    if(step === 6){
+        actions = true
+        $('.content-page4').hide('slow', function(){
+            $('.content-page5').show('slow', function(){
+                document.getElementById("plotting_canvas").style.setProperty('background-color', '#73c0ba')
+                lottie.loadAnimation({
+                    container: document.getElementById('5_instrucciones'),
+                    renderer: 'svg',
+                    loop: false,
+                    autoplay: true,
+                    path: '5_instrucciones.json'
+                })
+                actions = false
+                step = 7
+                if(step === 7){
+                    actions = true
+                    setTimeout(function(){
+                        $('.content-page5').hide('slow', function () {
+                            document.getElementById("plotting_canvas").style.setProperty('background-color', '#ffffff')
+                            // Logica de pincel aca, cuando seleccione ponga la función
+                            pincel = pincel11
+                            pincel.init()
+                            pintando = true;
+                            console.log('pintando', pintando)
+                            actions = false
+                            step = 8
+                            if(step === 8){
+                                actions = true
+                                setTimeout(function(){
+                                    webgazer.pause()
+                                    $('.content-page6').show('slow', function(){
+                                        lottie.loadAnimation({
+                                            container: document.getElementById('6_gracias'),
+                                            renderer: 'svg',
+                                            loop: false,
+                                            autoplay: true,
+                                            path: '6_gracias.json'
+                                        })
+                                        setTimeout(function(){
+                                            const mc = document.querySelector("#plotting_canvas");
+                                            $('.page-btn5').on('click', function(){
+                                                if(window.navigator.msSaveBlob){
+                                                    window.navigator.msSaveBlob(mc.msToBlob(), "archivo.jpeg");
+                                                } else{
+                                                    const a = document.createElement("a");
+                                                    document.body.appendChild(a);
+                                                    a.href = mc.toDataURL();
+                                                    a.download = "archivo.jpeg";
+                                                    a.click();
+                                                    document.body.removeChild(a);
+                                                }
+                                            })
+                                        }, 7000)
+                                    })
+                                }, 30000)
+                            }
+                        })
+                    }, 9000)
+                    actions = false
+                    step = 8
+                }
+            })
+        })
+    }
+})
+
+$('.ojo_optimista').on('click', function(){
+    if(step === 6){
+        actions = true
+        $('.content-page4').hide('slow', function(){
+            $('.content-page5').show('slow', function(){
+                document.getElementById("plotting_canvas").style.setProperty('background-color', '#73c0ba')
+                lottie.loadAnimation({
+                    container: document.getElementById('5_instrucciones'),
+                    renderer: 'svg',
+                    loop: false,
+                    autoplay: true,
+                    path: '5_instrucciones.json'
+                })
+                actions = false
+                step = 7
+                if(step === 7){
+                    actions = true
+                    setTimeout(function(){
+                        $('.content-page5').hide('slow', function () {
+                            document.getElementById("plotting_canvas").style.setProperty('background-color', '#ffffff')
+                            // Logica de pincel aca, cuando seleccione ponga la función
+                            pincel = pincel14
+                            pincel.init()
+                            pintando = true;
+                            console.log('pintando', pintando)
+                            actions = false
+                            step = 8
+                            if(step === 8){
+                                actions = true
+                                setTimeout(function(){
+                                    webgazer.pause()
+                                    $('.content-page6').show('slow', function(){
+                                        lottie.loadAnimation({
+                                            container: document.getElementById('6_gracias'),
+                                            renderer: 'svg',
+                                            loop: false,
+                                            autoplay: true,
+                                            path: '6_gracias.json'
+                                        })
+                                        setTimeout(function(){
+                                            const mc = document.querySelector("#plotting_canvas");
+                                            $('.page-btn5').on('click', function(){
+                                                if(window.navigator.msSaveBlob){
+                                                    window.navigator.msSaveBlob(mc.msToBlob(), "archivo.jpeg");
+                                                } else{
+                                                    const a = document.createElement("a");
+                                                    document.body.appendChild(a);
+                                                    a.href = mc.toDataURL();
+                                                    a.download = "archivo.jpeg";
+                                                    a.click();
+                                                    document.body.removeChild(a);
+                                                }
+                                            })
+                                        }, 7000)
+                                    })
+                                }, 30000)
+                            }
+                        })
+                    }, 9000)
+                    actions = false
+                    step = 8
+                }
+            })
+        })
+    }
+})
 
 function drawPixels(ctx, x, y) {
     for (var i = -10; i < 10; i += 4) {
@@ -308,69 +558,3 @@ var pincel15 = {
         }
     }
 }
-
-/*
-setTimeout(function () {
-    $('.content-page1 .text1').hide('slow', function () {
-        $('.content-page1 .text2').show('slow', function () {
-            actions = false
-            step = 2
-            if (step === 2) {
-                setTimeout(function () {
-                    $('.content-page1 .text2').hide('slow', function () {
-                        $('.content-page1 .text3').show('slow', function () {
-                            actions = false
-                            step = 3
-                        })
-                        $('.content-page1 .form, .content-page1 .nombreing, .content-page1 .page-btn').slideDown('slow', function () {
-                        })
-                    })
-                }, 500)
-            }
-        })
-    })
-}, 500)
-
-$('#btn-continuar').on('click', function () {
-    if (step === 3) {
-        if (!$('#name').val()) return;
-
-        actions = true
-        $('body').addClass('page2')
-        document.getElementById("page1id").setAttribute("style", "opacity:0.5; -moz-opacity:0.5; filter:alpha(opacity=50)");
-        $('.content-page2 .page-btn2').hide()
-        $('.content-page2').show('slow', function () {
-            $('#terms').on('change', function () {
-                if (this.checked) {
-                    $('.content-page2 .page-btn2').slideDown('slow', function () {
-                        actions = false
-                        step = 4
-                    })
-                    $('.content-page2 .dot').show()
-                } else {
-                    $('.content-page2 .page-btn2').slideToggle('fast')
-                    $('.content-page2 .dot').hide()
-                }
-            })
-        })
-    }
-})
-
-$('#btn-continuar2').on('click', function () {
-    if (step === 4) {
-        actions = true;
-        $('body').addClass('page3')
-        $('.content-page1').hide('slow')
-        $('.content-page2').hide('slow', function () {
-            $('.content-page3').show('slow', function () {
-                actions = false
-                step = 5
-            })
-        })
-    }
-})
-
-
-
-
-*/
